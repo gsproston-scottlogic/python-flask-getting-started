e5aba3b06f3a4e4703e8bb3be65553bb4d1017e6
George Sproston <gsproston@scottlogic.com>
2022-12-14 15:42:53 +0000

    Jinja templates

--- flashcards.py
+++ flashcards.py
@@ -1,20 +1,8 @@
-from datetime import datetime
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
-pageViews = 0
 
 
 @app.route("/")
 def welcome():
-  return "Welcome to my Flash Cards application!"
-
-@app.route("/date")
-def date():
-  return "This page was served at " + str(datetime.now())
-
-@app.route("/views")
-def views():
-  global pageViews
-  pageViews += 1
-  return "This page has been viewed " + str(pageViews) + " times"
+  return render_template("welcome.html")
