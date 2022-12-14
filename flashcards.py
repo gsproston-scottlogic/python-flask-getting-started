409e6f4ed034a04c7ad972a87ce6695bae358320
George Sproston <gsproston@scottlogic.com>
2022-12-14 15:47:55 +0000

    Jinja variables

--- flashcards.py
+++ flashcards.py
@@ -5,4 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def welcome():
-  return render_template("welcome.html")
+  return render_template(
+    "welcome.html", 
+    message="Here's a message from the view."
+  )
